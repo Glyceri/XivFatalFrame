e89274f347a1334e7e1c25b202c68d935f931de6
Amber Kortier <AmberKortier@gmail.com>
2025-05-14 14:47:33 +0200

    helpers for little Glyceri

--- XivFatalFrame/SteamAPI/Screenshot/SteamScreenshot.cs
+++ XivFatalFrame/SteamAPI/Screenshot/SteamScreenshot.cs
@@ -4,6 +4,9 @@ namespace XivFatalFrame.SteamAPI.Screenshot;
 
 [StructLayout(LayoutKind.Explicit, Pack = 1)]
 internal unsafe partial struct SteamScreenshot
-{ 
+{
+#define STEAMSCREENSHOTS_INTERFACE_VERSION "STEAMSCREENSHOTS_INTERFACE_VERSION003"
 
+    //https://github.com/rlabrecque/Steamworks.NET/blob/6b097c14dcf52cf139c79f8d882e5c87a4a6efb7/CodeGen/steam/isteamscreenshots.h 
+    // teehee
 }
